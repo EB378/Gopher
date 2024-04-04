68bf2289cceebb0f8ac585422c3c478e132b1c9d
EAlexBen <107051871+EB378@users.noreply.github.com>
2024-04-04 11:50:04 +0000

    modified:   src/pages/about.js 	modified:   src/pages/contact.js 	modified:   src/pages/news.js 	modified:   src/pages/pricing.js

---
+++ src/pages/pricing.js
@@ -0,0 +1,16 @@
+import styles from "@/app/page.module.css";
+import Nav from '@/app/components/Nav';
+import "@/app/globals.css";
+
+export default () => (
+    <>
+        <main className={styles.main}>
+            <div require className="App">
+                <header className="App-header">
+                    <Nav/>
+                </header>
+                <p>Pricing</p>
+            </div>
+        </main>
+    </>
+);
