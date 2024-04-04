386a65db06bc5516f0186a112de0fcfeae6f7956
EAlexBen <107051871+EB378@users.noreply.github.com>
2024-04-04 11:46:33 +0000

    modified:   src/app/components/Nav.js 	new file:   src/pages/about.js 	new file:   src/pages/contact.js 	new file:   src/pages/news.js 	new file:   src/pages/pricing.js 	new file:   src/pages/testamonials.js

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,14 +1,17 @@
 import styles from './Nav.module.css'
 import Logo from "./Logo";
+import Link from 'next/link'
 export default function Nav() {
   return (
     <>
       <div className={styles.topnav}>
           <Logo />
-          <a href="#home">Gopher</a>
-          <a href="#news">News</a>
-          <a href="#contact">Contact</a>
-          <a href="#about">About</a>
+          <Link href="/">Imperium</Link>
+          <Link href="/news">News</Link>
+          <Link href="/pricing">Pricing</Link>
+          <Link href="/contact">Contact</Link>
+          <Link href="/about">About</Link>
+          <Link href="/testamonials">Testamonials</Link>
       </div>
     </>  
   )
